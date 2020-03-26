329fab8f213989f43e66e2491a0296252bb3f7fe
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-03-26 13:18:44 -0500

    part one

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Awesome images!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://petguidereviews.com/wp-content/uploads/2019/02/cartoon-dog-names-768x461.jpg';
+  image3 = 'https://previews.123rf.com/images/siridhata/siridhata1702/siridhata170200078/71854477-cartoon-dog-vector-illustration.jpg';
 
   constructor() { }
 
